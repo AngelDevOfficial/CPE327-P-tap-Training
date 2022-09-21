a7b483f2c61bda8b283435d0946f46c538f6ed5b
Ball <ball.fiy555@gmail.com>
2022-09-21 17:09:02 +0700

    test

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
+import {  Container, Grid, GridItem } from "@chakra-ui/react"
+import Searchbar from "../components/Searchbar"
+import Navbar from "../components/Navbar"
+
 
 export default function Home() {
   return (
-    <div>
+    <Grid gap= {4}>
+      <GridItem>
+        <Navbar/>
+      </GridItem>
+      <GridItem>
+      <Searchbar/>
+      </GridItem>
 
-    </div>
+    </Grid>
+      
+    
   )
 }
