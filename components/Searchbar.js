0fb102a8511e312107baa3642362d140f024abb4
Ball <ball.fiy555@gmail.com>
2022-09-21 17:00:28 +0700

    searchbar edit

---
+++ components/Searchbar.js
@@ -0,0 +1,15 @@
+import {Box ,Input, Container, Button, Flex } from '@chakra-ui/react'
+
+
+export default function (){
+    return (
+        <Box>
+            <Flex>
+            <Input></Input>
+            <Button>Search</Button>
+            </Flex>
+            
+        </Box>
+    )
+
+}
